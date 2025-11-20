50218ec7433ed0e58525ce71114ea6da5d080020
unknown <helinconmx>
2025-11-21 00:15:58 +0530

    profile pic added

---
+++ frontend/src/components/menu/menus.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './menus.css'
+const Menus = () => {
+  return (
+    <>
+    <div className='navebar-profile-pic'>
+        <img src = "/my_profile_pic.jpg" alt = "Profile pic"/>
+    </div>
+    </>
+  )
+}
+
+export default Menus
